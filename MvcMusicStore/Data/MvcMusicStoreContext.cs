1696bc6ba9a4184867ae776570150e23cbfb9930
JKnight54321 <42787771+JKnight54321@users.noreply.github.com>
2018-09-12 23:54:56 -0600

    Attemt1

---
+++ MvcMusicStore/Data/MvcMusicStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMusicStore.Models
+{
+    public class MvcMusicStoreContext : DbContext
+    {
+        public MvcMusicStoreContext (DbContextOptions<MvcMusicStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMusicStore.Models.Song> Song { get; set; }
+    }
+}
---
+++ MvcMusicStore/Models/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMusicStore.Models
+{
+    public class Song
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genere { get; set; }
+
+        public string Album { get; set; }
+
+        public string Artist { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true )]
+        public DateTime ReleaseDate { get; set; }
+        
+    }
+}
